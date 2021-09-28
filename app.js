41e2d0681de4cbb0f9ee2238ca649a72bed80479
Sai-kishan-K <saik77556@gmail.com>
2021-09-28 21:06:41 +0530

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+function chatBox(){
+    document.getElementById("chat").style.display = "block";
+    document.getElementById("jcont").style.display = "none";
+    document.getElementById("htmlcont").style.display = "none";
+    document.getElementById("csscont").style.display = "none";
+}
+function show1(){
+    document.getElementById("jcont").style.display = "block";
+    document.getElementById("chat").style.display = "none";
+}
+function show2(){
+    document.getElementById("htmlcont").style.display = "block";
+    document.getElementById("chat").style.display = "none";
+}
+function show3(){
+    document.getElementById("csscont").style.display = "block";
+    document.getElementById("chat").style.display = "none";
+}
